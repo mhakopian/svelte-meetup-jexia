125f024df3147c717fb1d22c431cbd6234828516
Marlos Borges <marlos.borges@gmail.com>
2020-01-21 20:35:45 +0100

    add meetup markup
    copied from https://github.com/roeeyn/MeetupClone

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,87 @@
 <script>
-	export let name;
+  import Header from './UI/Header.svelte'
+  import MeetupGrid from './Meetups/MeetupGrid.svelte'
+  import TextInput from './UI/TextInput.svelte'
+  import Button from './UI/Button.svelte'
+  import EditMeetup from './Meetups/EditMeetup.svelte'
+
+  let meetups = [
+    {
+      id: 'm1',
+      title: 'Coding Bootcamp',
+      subtitle: 'Learn to code in 2 hours',
+      description:
+        'In this meetup, we will have some experts that teach you how to code!',
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Caffe_Nero_coffee_bar%2C_High_St%2C_Sutton%2C_Surrey%2C_Greater_London.JPG/800px-Caffe_Nero_coffee_bar%2C_High_St%2C_Sutton%2C_Surrey%2C_Greater_London.JPG',
+      address: '27th Nerd Road, 32523 New York',
+      contactEmail: 'code@test.com',
+      isFavorite: false
+    },
+    {
+      id: 'm2',
+      title: 'Swim Together',
+      subtitle: 'Let's go for some swimming',
+      description: 'We will simply swim some rounds!',
+      imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Olympic_swimming_pool_%28Tbilisi%29.jpg/800px-Olympic_swimming_pool_%28Tbilisi%29.jpg',
+      address: '27th Nerd Road, 32523 New York',
+      contactEmail: 'swim@test.com',
+      isFavorite: false
+    }
+  ];
+
+  let editMode;
+
+  function addMeetup(event) {
+    const newMeetup = {
+      id: Math.random().toString(),
+      title: event.detail.title,
+      subtitle: event.detail.subtitle,
+      description: event.detail.description,
+      imageUrl: event.detail.imageUrl,
+      contactEmail: event.detail.email,
+      address: event.detail.address
+    };
+
+    // meetups.push(newMeetup); // DOES NOT WORK!
+    meetups = [newMeetup, ...meetups];
+    editMode = null;
+  }
+
+  function cancelEdit() {
+    editMode = null;
+  }
+
+  function toggleFavorite(event) {
+    const id = event.detail;
+    const updatedMeetup = { ...meetups.find(m => m.id === id) };
+    updatedMeetup.isFavorite = !updatedMeetup.isFavorite;
+    const meetupIndex = meetups.findIndex(m => m.id === id);
+    const updatedMeetups = [...meetups];
+    updatedMeetups[meetupIndex] = updatedMeetup;
+    meetups = updatedMeetups;
+  }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    margin-top: 5rem;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .meetup-controls {
+    margin: 1rem;
+  }
 </style>
+
+<Header />
+
+<main>
+  <div class="meetup-controls">
+    <Button on:click={() => (editMode = "add")}>New Meetup</Button>
+  </div>
+  {#if editMode === "add"}
+    <EditMeetup on:save={addMeetup} on:cancel={cancelEdit} />
+  {/if}
+  <MeetupGrid {meetups} on:togglefavorite={toggleFavorite} />
+</main>
